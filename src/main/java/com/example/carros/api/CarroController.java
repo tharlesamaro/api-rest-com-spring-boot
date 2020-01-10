8cda6ddf65ff72dfe553857938cc969ee09f0990
Tharles Amaro <tharlesamaro@gmail.com>
2020-01-09 22:54:19 -0300

    @Service - Injeção de dependências com Spring

--- src/main/java/com/example/carros/api/CarroController.java
+++ src/main/java/com/example/carros/api/CarroController.java
@@ -2,6 +2,7 @@ package com.example.carros.api;
 
 import com.example.carros.domain.Carro;
 import com.example.carros.domain.CarroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/carros")
 public class CarroController {
-    private CarroService service = new CarroService();
+    @Autowired
+    private CarroService service;
 
     @GetMapping()
     public List<Carro> get() {
--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -1,8 +1,11 @@
 package com.example.carros.domain;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarroService {
     public List<Carro> getCarros() {
         List<Carro> carros = new ArrayList<>();
