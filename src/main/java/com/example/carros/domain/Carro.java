de1f1493f203f3a2bed30c2340b156fe92733eff
Tharles Amaro <tharlesamaro@gmail.com>
2020-01-12 13:44:47 -0300

    Adicionando a denpendência Lombok ao projeto

--- src/main/java/com/example/carros/domain/Carro.java
+++ src/main/java/com/example/carros/domain/Carro.java
@@ -1,11 +1,14 @@
 package com.example.carros.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "carro")
+@Data
 public class Carro {
 
     @Id
@@ -14,36 +17,4 @@ public class Carro {
 
     private String nome;
     private String tipo;
-
-    public Carro() {
-    }
-
-    public Carro(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
 }
