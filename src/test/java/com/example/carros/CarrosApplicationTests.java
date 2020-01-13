f345571f5a12f5dd8cdb44d98ce615775d7803d3
Tharles Amaro <tharlesamaro@gmail.com>
2020-01-12 23:55:55 -0300

    Escrevendo teste para inserir e deletar um carro

--- src/test/java/com/example/carros/CarrosApplicationTests.java
+++ src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -2,28 +2,46 @@ package com.example.carros;
 
 import com.example.carros.domain.Carro;
 import com.example.carros.domain.CarroService;
+import com.example.carros.domain.dto.CarroDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Optional;
 
 @SpringBootTest
 class CarrosApplicationTests {
 
-	@Autowired
-	private CarroService service;
+    @Autowired
+    private CarroService service;
 
-	@Test
-	public void inserirCarroTest() {
+    @Test
+    public void inserirCarro() {
 
-		Carro carro = new Carro();
-		carro.setNome("Ferrari Teste");
-		carro.setTipo("esportivos");
+        Carro carro = new Carro();
+        carro.setNome("Ferrari Teste");
+        carro.setTipo("esportivos");
 
-		service.insert(carro);
-	}
+        CarroDTO carroDTO = service.insert(carro);
+        Assertions.assertNotNull(carroDTO);
 
-	@Test
-	void contextLoads() {
-	}
+        Long id = carroDTO.getId();
+        Assertions.assertNotNull(id);
+
+        Optional<CarroDTO> optionalCarroDTO = service.getCarroById(id);
+        Assertions.assertTrue(optionalCarroDTO.isPresent());
+
+        carroDTO = optionalCarroDTO.get();
+        Assertions.assertEquals("Ferrari Teste", carroDTO.getNome());
+        Assertions.assertEquals("esportivos", carroDTO.getTipo());
+
+        service.delete(id);
+        Assertions.assertFalse(service.getCarroById(id).isPresent());
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
