54a78fdaec9fa4ebd66d4007fe7fb6de7f96db56
Tharles Amaro <tharlesamaro@gmail.com>
2020-01-12 22:48:53 -0300

    Iniciando teste para inserir um carro

--- src/test/java/com/example/carros/CarrosApplicationTests.java
+++ src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -1,10 +1,26 @@
 package com.example.carros;
 
+import com.example.carros.domain.Carro;
+import com.example.carros.domain.CarroService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarrosApplicationTests {
+
+	@Autowired
+	private CarroService service;
+
+	@Test
+	public void inserirCarroTest() {
+
+		Carro carro = new Carro();
+		carro.setNome("Ferrari Teste");
+		carro.setTipo("esportivos");
+
+		service.insert(carro);
+	}
 
 	@Test
 	void contextLoads() {
