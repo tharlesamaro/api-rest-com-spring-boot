4a76584facae24c0282cc8a9583837c0160047a5
Tharles Amaro <tharlesamaro@gmail.com>
2020-01-22 21:50:42 -0300

    Database Authentication buscando roles

--- src/main/java/com/example/carros/api/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/carros/api/security/UserDetailsServiceImpl.java
@@ -1,12 +1,11 @@
 package com.example.carros.api.security;
 
+import com.example.carros.domain.User;
 import com.example.carros.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service(value = "userDetailsService")
@@ -17,13 +16,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        com.example.carros.domain.User user = userRepository.findByLogin(username);
+        User user = userRepository.findByLogin(username);
         if (user == null) {
             throw new UsernameNotFoundException("user not found");
         }
 
-        return User.withUsername(username).password(user.getSenha()).roles("USER").build();
+        return user;
     }
 }
