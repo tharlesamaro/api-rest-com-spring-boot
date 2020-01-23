dcb8ab9362a079b88029e93dd7be6caeec568a49
Tharles Amaro <tharlesamaro@gmail.com>
2020-01-22 21:23:43 -0300

    Inicnando a autenticação com banco de dados

---
+++ src/main/java/com/example/carros/domain/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.carros.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByLogin(String login);
+}
